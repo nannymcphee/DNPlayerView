6839b4c9f35020efbf08f9b860005e46a6119704
Duy Nguyen <victornguyen0705@gmail.com>
2022-05-01 10:42:30 +0700

    Init project

---
+++ DNPlayerView/DNPlayerView.h
@@ -0,0 +1,18 @@
+//
+//  DNPlayerView.h
+//  DNPlayerView
+//
+//  Created by Duy Nguyen on 01/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DNPlayerView.
+FOUNDATION_EXPORT double DNPlayerViewVersionNumber;
+
+//! Project version string for DNPlayerView.
+FOUNDATION_EXPORT const unsigned char DNPlayerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DNPlayerView/PublicHeader.h>
+
+
